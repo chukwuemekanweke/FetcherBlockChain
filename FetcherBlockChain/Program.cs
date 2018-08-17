e26386988bfe914d934b70ed35abdaa40dae494e
jeremia <emekanweke604@gmail.com>
2018-08-17 16:58:23 +0100

    Add project files.

---
+++ FetcherBlockChain/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FetcherBlockChain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
